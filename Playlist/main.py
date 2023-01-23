932975b5cce823cdddfac1b86f68a1462ae3e02c
KFkas <48663613+JaoKFkas@users.noreply.github.com>
2023-01-23 08:19:40 -0300

    Update main.py

--- Playlist/main.py
+++ Playlist/main.py
@@ -3,7 +3,6 @@ import mp3_player
 # Playlist
 
 playlist_url = str(input("Insert your playlist url: "));
-playlist_url = "https://www.youtube.com/playlist?list=PLTRNoDrjZ3DIXjiPZmaaq4JieLnkKAsDi";
 
 while True:
     mp3_player.Start(playlist_url);
